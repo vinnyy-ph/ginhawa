2b7609d08d914222c871f0e752c14c41e1758802
Vincent Ferrer <113322585+vinnyy-ph@users.noreply.github.com>
2026-05-26 22:18:25 +0800

    feat: add config module and local storage env vars

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,4 +1,5 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { APP_GUARD } from '@nestjs/core';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
@@ -12,6 +13,7 @@ import { RolesGuard } from './auth/guards/roles.guard';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
     PrismaModule,
     UsersModule,
     AuthModule,
