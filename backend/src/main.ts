b2e86dbc0eecb54a0c139a3ee9418528cae94f96
Vincent Ferrer <113322585+vinnyy-ph@users.noreply.github.com>
2026-05-28 12:02:52 +0800

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -12,6 +12,6 @@ async function bootstrap() {
   app.useStaticAssets(join(process.cwd(), 'uploads'), {
     prefix: '/uploads/',
   });
-  await app.listen(process.env.PORT ?? 3001);
+  await app.listen(process.env.PORT ?? 3001, '0.0.0.0');
 }
 void bootstrap();
